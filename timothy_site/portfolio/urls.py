324779d28afc368f0a5fa2450dce9a41a193a3c2
WildCoder🍂 <118418723+Timothyke@users.noreply.github.com>
2025-06-26 09:00:26 +0300

    Update urls.py

--- timothy_site/portfolio/urls.py
+++ timothy_site/portfolio/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('download-cv/', views.download_cv, name='download_cv'),
+]
+
 urlpatterns = [
     path('', views.index, name='index'),
 ]

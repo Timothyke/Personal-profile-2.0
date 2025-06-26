d412a689fbf63047ec3e6a9c96c4466682668104
WildCoder🍂 <118418723+Timothyke@users.noreply.github.com>
2025-06-26 09:01:25 +0300

    Update models.py

--- timothy_site/portfolio/models.py
+++ timothy_site/portfolio/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib import admin
+from .models import DownloadLog
+
+admin.site.register(DownloadLog)
 
 class DownloadLog(models.Model):
     ip_address = models.GenericIPAddressField()

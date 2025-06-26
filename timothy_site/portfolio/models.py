204e07c211b5ca961115a054a6a5980a08a9822b
WildCoder🍂 <118418723+Timothyke@users.noreply.github.com>
2025-06-26 08:56:54 +0300

    Update models.py

--- timothy_site/portfolio/models.py
+++ timothy_site/portfolio/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class DownloadLog(models.Model):
+    ip_address = models.GenericIPAddressField()
+    user_agent = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.ip_address} at {self.timestamp}"
